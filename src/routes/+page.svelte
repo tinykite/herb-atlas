8757ed5d50f4102c0554184c100ddb110965af42
tinykite <dakota@tinykitelab.com>
2024-07-26 16:10:37 -0500

    Add POC Google sheet load function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+	import * as d3 from 'd3';
+
+	let formattedData = [];
+	const csvUrl =
+		'https://docs.google.com/spreadsheets/d/1hgFHSzxmGEZ0oiJLnalemEr6vp_nHd15m-jc5EH2ahY/gviz/tq?tqx=out:csv';
+
+	const fetchData = async () => {
+		try {
+			const data = await d3.csv(csvUrl, (data) => {
+				// const farmName = d3.csvParse(data.Farm);
+				// formattedData.push(farmName);
+				formattedData.push(data);
+			});
+			console.log(formattedData);
+		} catch (error) {
+			console.error('Error fetching or parsing CSV data:', error);
+		}
+	};
+
+	onMount(() => {
+		fetchData();
+	});
+</script>
