183acdf6afd3914656a574d3d9e5d04af0e47c34
tinykite <dakota@tinykitelab.com>
2024-11-30 14:53:11 -0600

    Add logo and website title

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	// Importing this in the layout, instead of app.html, to enable HMR
+	import '../styles/global.css';
+
+    interface Props {
+		children?: import('svelte').Snippet;
+	}
+
+	let { children }: Props = $props();
+</script>
+
+<main class="wrapper">
+{@render children?.()}
+</main>
