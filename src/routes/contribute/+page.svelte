7762008338fef80674f8c1e55c77f5af9405f9ad
tinykite <dakota@tinykitelab.com>
2024-12-31 16:36:14 -0600

    Add contribute and donate pages

---
+++ src/routes/contribute/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+	import Nav from '../../components/Nav.svelte';
+	import Map from '../../components/Map.svelte';
+	import { DEFAULT_MAP_CENTER } from '$lib/mapData';
+</script>
+
+<Nav hideSearch={true} />
+<main class="main">
+	<h1>Contribute</h1>
+</main>
+
+<Map mapPoints={[]} mapCenter={DEFAULT_MAP_CENTER} />
+
+<style>
+	.main {
+		grid-column: 1 / -1;
+		max-width: 55vw;
+		margin-block: 2rem;
+		margin-inline: auto;
+		color: #397740;
+		background: #faf6d4;
+		position: relative;
+		z-index: 300;
+		padding: 1.5rem;
+		border-radius: 1rem;
+		box-shadow:
+			0 100px 80px 0 rgba(0, 0, 0, 0.02),
+			0 22px 18px 0 rgba(0, 0, 0, 0.04),
+			0 6px 5px 0 rgba(0, 0, 0, 0.08);
+	}
+
+	.main * + * {
+		margin-block-start: 0.5rem;
+	}
+</style>
