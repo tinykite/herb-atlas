22d0dffe46850b8cfb55b98eda42abfe3f3f9666
tinykite <dakota@tinykitelab.com>
2025-01-09 09:53:45 -0600

    Update about page content

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -10,33 +10,32 @@
 <main class="main">
 	<h1 class="main__title">About</h1>
 	<p class="main__text">
-		I started Herb Atlas out of a desire to better visualize for myself how to practice more
-		sustainable, bioregional herbalism — a term with roots in the bioregionalism movement and the
-		activism of environmentalist Peter Berg.
+		Herb Atlas is an interactive map that highlights small, human-scale farms growing medicinal
+		herbs with a committment to sustainble and regenerative farming practices. The project was
+		inspired by a desire to better familiarize myself with bioregionalism herbalism — a term with
+		roots in the bioregionalism movement and the activism of environmentalist Peter Berg.
 	</p>
 	<p class="main__text">
+		There are no exact definitions of what bioregional herbalism is or has to be. But quite simply,
 		I wanted to make it easy for myself, and others, to discover farms growing and selling plants
-		that are weedy, abundant, or otherwise well-adapted to a region closer to their home. In the
-		process, I'm hoping it also serves as a way to learn about and support sustainable practices
-		like <a
-			href="https://www.climatehubs.usda.gov/hubs/international/topic/no-till-farming-climate-resilience"
-			>no-till farming</a
-		>, an important strategy for climate resilience.
-	</p>
-
-	<p class="main__text">
-		By buying from small, human-scale farms, I've additionally gained much more awareness about how
-		to assess the quality of dried herbs through their color, taste, and smell — and learned why it
-		matters. Which may feel like an obvious benefit — but when I first started buying medicinal
-		herbs, I didn't realize there was more to consider than simply whether or not something was
-		organic.
+		that are weedy, abundant, or otherwise well-adapted to a region closer to their home.
 	</p>
 
 	<p>
-		I didn't realize that it's abnormal for dried herbs to smell like the dusty pages of an old
-		book. Or that it's not normal for a floral member of the mint family to taste kind of
-		off-puttingly woody.
+		In the process, I'm hoping this website also serves as a way to learn about and support farms
+		with a committment to sustainable practices that promote climate resilience, such as <a
+			href="https://www.climatehubs.usda.gov/hubs/international/topic/no-till-farming-climate-resilience"
+			>no-till farming</a
+		>. Buying from human-scale farms can additionally teach you to better understand color, smell,
+		and taste as key indicator's of an herb's quality.
 	</p>
+
+	<p>
+		Which may all feel obvious — but when I first started buying medicinal herbs, I had no idea
+		there was more to consider than simply whether or not something was organic. This project hopes
+		to make that just a little easier to understand.
+	</p>
+
 	<h2 class="main__subheading">Contribute</h2>
 	<p class="main__text">
 		Anyone is welcome to <a
