8b6b71f2723381d68be6441e76854dc7b1ca5b83
tinykite <dakota@tinykitelab.com>
2025-01-10 09:52:15 -0600

    Add link icon to external links

--- src/components/Locations.svelte
+++ src/components/Locations.svelte
@@ -117,8 +117,22 @@
 				{/if}
 			</p>
 
-			<p class="location__text">
-				<a href={filteredResults[0].Url}>Visit {filteredResults[0].Name} Website</a>
+			<p class="location__text link">
+				<svg
+					class="link__icon"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 24 24"
+					fill="none"
+					aria-hidden="true"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71" /><path
+						d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"
+					/></svg
+				>
+				<a href={filteredResults[0].Url}>{filteredResults[0].Name} Website</a>
 			</p>
 		</div>
 	{/if}
@@ -229,5 +243,16 @@
 		font-size: 1rem;
 		line-height: 1.2;
 		font-style: italic;
+	}
+
+	.link {
+		display: flex;
+		align-items: center;
+		gap: 0.5rem;
+	}
+
+	.link__icon {
+		height: 1rem;
+		width: auto;
 	}
 </style>
