b42788f222901ca7eaa3dffd2c440191441f840c
tinykite <dakota@tinykitelab.com>
2025-01-14 10:33:15 -0600

    Update about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -31,18 +31,40 @@
 	<p>
 		Which may all feel obvious — but when I first started buying medicinal herbs, I had no idea
 		there was more to consider than simply whether or not something was organic. This project hopes
-		to make that just a little easier to understand.
+		to make that a little easier.
 	</p>
 
-	<h2 class="main__subheading">Contribute</h2>
-	<p class="main__text">
-		Anyone is welcome to <a
-			href="https://docs.google.com/forms/d/e/1FAIpQLSe9M3cPNsPAWdqSbASt-eMfCddZspQhYPdB1XRocxvQDnLD5g/viewform?usp=sharing"
-			>suggest a farm</a
-		> to add to the map. Farm submissions will be reviewed before being added. Each farm submitted should
-		be committed to regenerative farming practices.
+	<h2 class="main__subheading">Land Acknowledgment</h2>
+	<p>
+		This website was created in Minneapolis, in Mni Sóta Makoce. As the <a
+			href="https://nativegov.org/about/our-land-acknowledgement-statement/"
+			>Native Governence Center</a
+		>
+		so well describes,
+	</p>
+	<blockquote>
+		"Mni Sota Makoce (Minnesota) is the homeland of the Dakota people. The Dakota have lived here
+		for many thousands of years. Anishinaabe people reside here, too, and reached their current
+		homelands after following the megis shell to the food that grows on water (manoomin, or wild
+		rice). Indigenous people from other Native nations also reside in Minnesota and have made
+		innumerable contributions to our region."
+	</blockquote>
+
+	<p>
+		It feels important to consider the past, present, and future of the land I occupy, but I
+		understand that acknowledgment alone is not enough — it is only a starting place. As <a
+			href="https://www.queeringthemap.com">Queering the Map</a
+		>
+		puts it (emphasis mine), "It is pertinent that we continuously reflect <em>and act</em>
+		on the ways in which LGBTQIA+ life is complicit in ongoing processes of settler-colonialism."
 	</p>
 
+	<p>
+		<a href="https://native-land.ca">Native Land Digital</a> maintains an interactive map as a public
+		space where "non-Indigenous people can be invited and challenged to learn more about the lands they
+		inhabit, the history of those lands, and how to actively be part of a better future going forward
+		together."
+	</p>
 	<h2 class="main__subheading">Contact</h2>
 	<p class="main__text">
 		Herb Atlas is a project created by Dakota Sexton. Please feel free to <a
